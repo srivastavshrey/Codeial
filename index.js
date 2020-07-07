c67aef74b1b2782dbd8f0ead55e11f338742428b
srivastavshrey <52007374+srivastavshrey@users.noreply.github.com>
2020-07-07 21:53:56 +0530

    Rendered HTML page

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 8000;
-app.use('/', require('./routes/index'))
+app.set('view engine','ejs');
+app.set('views','./views');
+app.use(express.urlencoded());
+app.use('/', require('./routes/index'));
 app.listen(port,function(err){
     if(err){
         console.log(`Error!:${err}`);
