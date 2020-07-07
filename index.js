c1de03033f1fcf7a5bb3ed2857ec5340404e2f07
srivastavshrey <52007374+srivastavshrey@users.noreply.github.com>
2020-07-08 01:00:40 +0530

    Added static files

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const expressLayouts = require("express-ejs-layouts");
 const port = 8000;
 app.set("view engine", "ejs");
 app.set("views", "./views");
+app.use(express.static("./assets"));
 app.use(expressLayouts);
+app.set("layout extractStyles", true);
+app.set("layout extractScripts", true);
 app.use("/", require("./routes/index"));
 app.listen(port, function (err) {
   if (err) {
