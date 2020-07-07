3f41563d4552848bc5b2d9a0f562ae8d89b2c1a2
srivastavshrey <52007374+srivastavshrey@users.noreply.github.com>
2020-07-08 00:33:08 +0530

    Added partials and layout

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const expressLayouts = require("express-ejs-layouts");
 const port = 8000;
-app.set('view engine','ejs');
-app.set('views','./views');
-
-app.use('/', require('./routes/index'));
-app.listen(port,function(err){
-    if(err){
-        console.log(`Error!:${err}`);
-        return;
-    }
-    console.log(`Server is running on port:${port}`);
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(expressLayouts);
+app.use("/", require("./routes/index"));
+app.listen(port, function (err) {
+  if (err) {
+    console.log(`Error!:${err}`);
+    return;
+  }
+  console.log(`Server is running on port:${port}`);
 });
